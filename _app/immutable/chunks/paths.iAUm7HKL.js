0d607ef2718b3bc43de1aadbb5f19dbb378390c4
DeveloperMikey <DeveloperMikey@users.noreply.github.com>
2025-01-01 13:05:42 +0000

    deploy: 291bdaae821eebd79ef1921b156e04075c9a9d0d

--- _app/immutable/chunks/paths.iAUm7HKL.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1se47gw)==null?void 0:s.base)??"/geograafia";var a;const t=((a=globalThis.__sveltekit_1se47gw)==null?void 0:a.assets)??e;export{t as a,e as b};
--- _app/immutable/entry/start.DmnknRLq.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.6gXnUKd2.js";export{t as start};
