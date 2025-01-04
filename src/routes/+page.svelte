987d6b045de184dce8d874147bfa84fc1c66417d
DeveloperMikey <maemeesmihkel@gmail.com>
2025-01-04 12:40:24 +0200

    Maastikute kaardid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,2 @@
 <a href="/geograafia/maps/veekogud">Veekogude kaart</a>
+<a href="/geograafia/maps/maastikud">Maastikute kaart</a>
---
+++ src/routes/maps/maastikud/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Map from "$lib/map.svelte";
+
+    const landforms = [
+        "Haanja kõrgustik",
+        "Harju lavamaa",
+        "Karula kõrgustik",
+        "Kesk-Eesti tasandik",
+        "Lääne-Eesti madalik",
+        "Otepää kõrgustik",
+        "Pandivere kõrgustik",
+        "Pärnu madalik",
+        "Peipsi madalik",
+        "Põhja-Eesti paekallas",
+        "Sakala kõrgustik",
+        "Ugandi lavamaa",
+        "Vooremaa",
+        "Võrtsjärve madalik",
+    ];
+</script>
+
+<Map path="maastikud" names={landforms} />
